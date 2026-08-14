343a0f7b9ef6fdda89549a185d9f76513672eae1
GJMthethinker <gillesjosemanuel9@gmail.com>
2026-08-14 19:59:14 -0400

    Refactor EventPage to use React.createElement

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { getEventBySlug } from "@/lib/data/events";
 import { listTicketTypesByEvent } from "@/lib/data/tickets";
 import EventPageClient from "@/app/events/[slug]/EventPageClient";
 import { notFound } from "next/navigation";
+import { createElement as h } from "react";
 
 export default async function EventPage({ params }: { params: { slug: string } }) {
     const event = await getEventBySlug(params.slug);
@@ -9,5 +10,5 @@ export default async function EventPage({ params }: { params: { slug: string } }
 
   const ticketTypes = await listTicketTypesByEvent(event.id);
 
-  return EventPageClient({ event, ticketTypes });
+    return h(EventPageClient, { event, ticketTypes });
 }
